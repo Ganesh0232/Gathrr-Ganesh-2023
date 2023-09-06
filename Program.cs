3f93b456071f72506f8b9a4d14b67e48de93a902
Ganesh Kumar Marrapu <ganeshkumar.marrapu+123@gmail.com>
2023-09-06 17:47:35 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Gathrr_Ganesh_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
